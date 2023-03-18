caecff5ab69d7811203d5e0f89ff033ca890afbf
Richard Anaya <richard.anaya@gmail.com>
2023-03-17 22:29:04 -0700

    comments

--- src/phase_2.rs
+++ src/phase_2.rs
@@ -1,6 +1,8 @@
 use crate::common::KeyPoint;
 
 fn is_corner(p: u8, circle: &[u8], threshold: u8) -> bool {
+    // In the context of image processing and computer vision, a "corner" refers to a point in the
+    // image where there is a significant change in intensity or color in multiple directions.
     let mut count = 0;
     let mut consecutive = 0;
 
@@ -33,6 +35,10 @@ const OFFSETS: [(i32, i32); 12] = [
     (-3, -1),
 ];
 
+// This function takes an image, its dimensions, and a threshold as input and returns a list of
+// keypoints (corners) in the image. It iterates over the image and for each pixel, it checks if
+// the pixel is a corner by comparing it to the pixels in a circle around it. If the pixel is a
+// corner, it is added to the list of keypoints.
 pub fn fast_keypoints(
     img: &[u8],
     width: usize,
@@ -56,6 +62,13 @@ pub fn fast_keypoints(
     keypoints
 }
 
+/* This function takes an image, its dimensions, and a list
+of keypoints (corners) as input and computes the dominant
+orientation of the gradient for each keypoint. It calculates
+the weighted sum of the gradients in a circle around each keypoint
+using the pixel values and the circle offsets. The final output is a
+vector of KeyPoint structures, each containing the x, y coordinates and
+orientation of the gradient for the corresponding keypoint. */
 pub fn compute_orientations(
     img: &[u8],
     width: usize,
